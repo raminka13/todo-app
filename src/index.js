52baa9df06e75f609aa236b358ffb46d1b095567
Raminka <raminka13@hotmail.com>
2022-07-05 16:00:39 -0500

    Webpack basic setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
